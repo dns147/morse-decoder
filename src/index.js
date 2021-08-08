bc920dcaa28a2db89eccc1ac77f3ae5b918c34e5
github-dns147 <rarden@ya.ru>
2021-08-08 19:38:02 +0300

    add: solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+  const makeLetterMorse = (letterBinary) => {
+    const arr = letterBinary.match(/.{1,2}/g);
+    let letterMorse = '';
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === '11') {
+        letterMorse += '-';
+      } else if (arr[i] === '10') {
+        letterMorse += '.';
+      }
+    }
+
+    return letterMorse;
+  }
+
+  let resStr = '';
+
+  for (let i = 0; i < expr.length; i += 10) {
+    const letterCut = expr.slice(i, i + 10);
+
+    if (letterCut === '**********') {
+      resStr += ' ';
+    } else {
+      const letter = makeLetterMorse(letterCut);
+      resStr += MORSE_TABLE[letter];
+    }
+  }
+
+  return resStr;
 }
 
 module.exports = {
